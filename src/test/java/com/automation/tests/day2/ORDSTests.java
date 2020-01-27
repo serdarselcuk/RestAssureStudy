fd35b3179e4210beed38d5c30b08b87232398b04
serdarselcuk <serdarselcukus@outlook.com>
2020-01-27 13:31:51 -0600

    adhanTime created in warmUp

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,11 +1,7 @@
 package com.automation.tests.day2;
-
-
-import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
