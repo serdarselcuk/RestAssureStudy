6a2cba872b1d7ee64803cf1ce0255face1de5de8
serdarselcuk <serdarselcukus@outlook.com>
2020-01-31 14:45:10 -0600

    last situation

---
+++ src/test/java/com/automation/pojos/Address.java
@@ -0,0 +1,73 @@
+package com.automation.pojos;
+
+public class Address {
+
+    private int addressId;
+    private String city;
+    private String state;
+    private String street;
+    private  int zipCode;
+
+    public Address(){}
+
+    public Address(int sddressId, String city, String state, String street, int zipCode) {
+        this.addressId = sddressId;
+        this.city = city;
+        this.state = state;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+
+    public int getSddressId() {
+        return addressId;
+    }
+
+    public void setSddressId(int sddressId) {
+        this.addressId = sddressId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "sddressId=" + addressId +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", street='" + street + '\'' +
+                ", zipCode=" + zipCode +
+                '}';
+    }
+}
+
+
