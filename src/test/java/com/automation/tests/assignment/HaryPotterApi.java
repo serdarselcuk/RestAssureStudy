6e2216feb7eba34d8a7f3a6b45b8519366bb038d
serdarselcuk <serdarselcukus@outlook.com>
2020-02-03 11:08:13 -0600

    harypotter has fixed

--- src/test/java/com/automation/tests/assignment/HaryPotterApi.java
+++ src/test/java/com/automation/tests/assignment/HaryPotterApi.java
@@ -5,6 +5,8 @@ import com.automation.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.apache.http.util.Asserts;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,16 +25,21 @@ public class HaryPotterApi {
     @Test
     @DisplayName("Verify Sorting Hat")
     public void Test1() {
-//?????????
+        Response response =
             when().
-                get("/sortingHat").prettyPeek().
+                get("/sortingHat");
+        response.
             then().
                 assertThat().
                 statusCode(200).
-                contentType("application/json; charset=utf-8")
-                .body("[0]", is(""));
+                contentType("application/json; charset=utf-8");
 
-        List<String> list = new ArrayList<String>(Arrays.asList("Slytherin", "Ravenclaw", "Hufflepuff", "Gryffindor"));
+        Assertions.assertTrue(List.of("Slytherin", "Ravenclaw", "Hufflepuff", "Gryffindor").
+                contains(response.getBody().
+                        asString().
+                        replace("\"","")),
+                "hat name is not recognized");
+
 
     }
 
@@ -43,9 +50,9 @@ public class HaryPotterApi {
         given().
                 accept(ContentType.JSON).
                 queryParam("key", "invalid").
-                when().
+            when().
                 get("/characters").
-                then().
+            then().
                 assertThat().
                 statusCode(401).
                 contentType("application/json; charset=utf-8").
@@ -60,9 +67,9 @@ public class HaryPotterApi {
 
         given().
                 accept(ContentType.JSON).
-                when().
+            when().
                 get("/characters").
-                then().
+            then().
                 assertThat().
                 statusCode(409).
                 contentType("application/json; charset=utf-8").
@@ -78,13 +85,13 @@ public class HaryPotterApi {
         given().
                 accept(ContentType.JSON).
                 queryParam("key", "$2a$10$kFn7hhS2HAIoBhQX8CcumOJhV58NFfKaunq83jLlLUZEEGwHIf2F.").
-                when().
+            when().
                 get("/characters").
-                then().
+            then().
                 assertThat().
                 statusCode(200).
                 contentType("application/json; charset=utf-8").
-                body("size()", hasSize(195));
+                body("", hasSize(195));
 
     }
 
@@ -95,9 +102,9 @@ public class HaryPotterApi {
         given().
                 accept(ContentType.JSON).
                 queryParam("key", "$2a$10$kFn7hhS2HAIoBhQX8CcumOJhV58NFfKaunq83jLlLUZEEGwHIf2F.").
-                when().
+            when().
                 get("/characters").
-                then().
+            then().
                 assertThat().
                 statusCode(200).
                 contentType("application/json; charset=utf-8").
@@ -115,22 +122,22 @@ public class HaryPotterApi {
                 accept(ContentType.JSON).
                 queryParam("key", "$2a$10$kFn7hhS2HAIoBhQX8CcumOJhV58NFfKaunq83jLlLUZEEGwHIf2F.").
                 queryParam("name", "Ludo Bagman").
-                when().
+            when().
                 get("/characters").
-                then().
-                    assertThat().
-                        statusCode(200).
-                        contentType("application/json; charset=utf-8").
-                        body("_id", hasItem("5a0fa5deae5bc100213c2330")).
-                        body("name", hasItem("Ludo Bagman")).
-                        body("role", hasItem("Head, Department of Magical Games and Sports")).
-                        body("__v", hasItem(0)).
-                        body("ministryOfMagic", hasItem(true)).
-                        body("orderOfThePhoenix", hasItem(false)).
-                        body("dumbledoresArmy", hasItem(false)).
-                        body("deathEater", hasItem(false)).
-                        body("bloodStatus",hasItem("unknown")).
-                        body("species",hasItem("human"));
+            then().
+                assertThat().
+                    statusCode(200).
+                    contentType("application/json; charset=utf-8").
+                    body("_id", hasItem("5a0fa5deae5bc100213c2330")).
+                    body("name", hasItem("Ludo Bagman")).
+                    body("role", hasItem("Head, Department of Magical Games and Sports")).
+                    body("__v", hasItem(0)).
+                    body("ministryOfMagic", hasItem(true)).
+                    body("orderOfThePhoenix", hasItem(false)).
+                    body("dumbledoresArmy", hasItem(false)).
+                    body("deathEater", hasItem(false)).
+                    body("bloodStatus",hasItem("unknown")).
+                    body("species",hasItem("human"));
     }
     @Test
     @DisplayName("Verify name search")
@@ -171,6 +178,7 @@ public class HaryPotterApi {
                 contentType("application/json; charset=utf-8").extract().jsonPath();
         List<Object>members=null;
         String houseId=null;
+
         for (int i = 0; i < 4; i++) {
             if (js.getString("name["+i+"]").equals("Gryffindor")){
                 houseId=js.getString("_id["+i+"]");
